6a2b6cba53df6dc69128581dd10c2ba3bfa9ac01
duorpeb <a01029397920@gmail.com>
2025-07-07 14:07:08 +0900

    topic_testDev_test2

--- src/main/java/com/bedrequest/www/test/Heachan.java
+++ src/main/java/com/bedrequest/www/test/Heachan.java
@@ -2,7 +2,7 @@ package com.bedrequest.www.test;
 
 public class Heachan {
     // test Heachan
-    private int hachan;
+    private String hahachan;
     // 머지 테스트
     // 확인해주세요.
     // 수정하기
--- src/main/java/com/bedrequest/www/test/LYHController.java
+++ src/main/java/com/bedrequest/www/test/LYHController.java
@@ -3,4 +3,6 @@ package com.bedrequest.www.test;
 // 이유현
 public class LYHController {
     private int lyh;
+
+    private String ysl = "Lee Young Seok";
 }
